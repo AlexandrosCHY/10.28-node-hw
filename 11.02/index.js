08da6bc11d7b63fab1074cde7ae036bca6ffa2ca
Alexandros Chysafidis <gorogalexalexgorog@gmail.com>
2023-11-04 11:26:47 +0100

    11.02 folder

---
+++ 11.02/index.js
@@ -0,0 +1,52 @@
+import calculator from "./calculator.js";
+import readlineModule from "readline";
+const { add, subtract, multiply, divide } = calculator;
+
+const readline = readlineModule.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question("What should we calculate? ", (answer) => {
+  const userCommand = answer.split(" ");
+  const operation = userCommand[0];
+  const operand1 = Number(userCommand[1]);
+  const operand2 = Number(userCommand[2]);
+
+  let result;
+  let operationSign;
+
+  if (isNaN(operand1) || isNaN(operand2)) {
+    console.error("Wrong parameters, try again");
+  } else {
+    switch (operation) {
+      case "add": {
+        operationSign = "+";
+        result = add(operand1, operand2);
+        break;
+      }
+      case "multiply": {
+        operationSign = "*";
+        result = multiply(operand1, operand2);
+        break;
+      }
+      case "divide": {
+        operationSign = "/";
+        result = divide(operand1, operand2);
+        break;
+      }
+      case "subtract": {
+        operationSign = "-";
+        result = subtract(operand1, operand2);
+        break;
+      }
+      default: {
+        console.log("Operation not permitted");
+      }
+    }
+
+    if (operationSign != null && result != null) {
+      console.log(`${operand1} ${operationSign} ${operand2} = ${result}`);
+    }
+  }
+});
